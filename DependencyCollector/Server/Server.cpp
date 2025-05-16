fd9ce4beaa8eb02ccbf06f4de1918093fe635adb
Константин Васильев <piskakosti@gmail.com>
2025-05-16 22:10:44 +0400

    init

--- DependencyCollector/Server/Server.cpp
+++ DependencyCollector/Server/Server.cpp
@@ -1,4 +1,5 @@
 #pragma once
+
 #include "httplib.h"
 #include "pch.h"
 
@@ -55,14 +56,12 @@ private:
             serializer.serialize(currentDependencies, fileName);
             res->status = 200;
             res->set_content("Graph was successfully saved", "text/plain");
-            return;
         }
         else 
         {
             //DWORD error = CommDlgExtendedError();
             res->status = 500;
             res->set_content("Graph was not saved", "text/plain");
-            return;
         }
     }
 
@@ -171,10 +170,8 @@ public:
             {
                 this->config = nlohmann::json::parse(f);
             }
-            dc::Reader reader;
-            vector<pair<string, vector<string>>> rawData = reader.getRawData(config["catalog"]); // <file_path, <raw includes>>
-            dc::Formater formater;
-            currentDependencies = formater.formatData(rawData); // <Node, <Links_To_Other_Nodes>>
+            vector<pair<string, vector<string>>> rawData = dc::Reader::getRawData(config["catalog"]); // <file_path, <raw includes>>
+            currentDependencies = dc::Formater::formatData(rawData); // <Node, <Links_To_Other_Nodes>>
             dc::Serializer serializer;
             auto json = serializer.serialize(currentDependencies);
             res.set_content(json, "text");
